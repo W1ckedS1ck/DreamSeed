1bc9ed368dbcddbbaa6056538e925c21da98dfae
W1ckedS1ck <noreply@github.com>
2026-05-15 12:54:07 +0300

    test: bump instance type to t3.large to demonstrate Infracost cost diff

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -53,7 +53,7 @@ data "aws_ami" "ubuntu" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.small"
+  default     = "t3.large"
 }
 
 variable "ssh_public_key_path" {
